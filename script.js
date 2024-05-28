5d4035d3453d18945b267198305410132d4318be
Hester D'Souza <hester.dsouza.08@gmail.com>
2024-05-28 22:32:38 +0530

    Updated commit. Added validation.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const changeColor = document.getElementsByTagName("input");
+document.addEventListener("DOMContentLoaded",() => {
+    const form = document.querySelector("#signUpForm");
+    const password = document.querySelector("#password");
+    const confirm_password = document.querySelector("#confirm_password");
 
-changeColor[0].addEventListener("click", {
-    border: "0062ff"
-})
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    form.addEventListener('submit', (e) => {
+        if (!passwordPattern.test(password.value)) {
+            e.preventDefault();
+            password.setCustomValidity("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.");
+        } else if(password.value !== confirm_password.value){
+            e.preventDefault();
+            confirm_password.setCustomValidity("Passowrds do not match");
+        } else {
+            confirm_password.setCustomValidity("");
+        }
+    });
+
+    password.addEventListener("input", ()=> {
+        if (!passwordPattern.test(password.value)) {
+            password.setCustomValidity("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.");
+        } else {
+            password.setCustomValidity("");
+        }
+        confirm_password.setCustomValidity(password.value !== confirm_password.value ? "Passwords do not match" : "");
+    });
+
+    confirm_password.addEventListener("input", ()=> {
+        confirm_password.setCustomValidity(password.value !== confirm_password.value ? "Passwords do not match" : "");
+    });
+});
